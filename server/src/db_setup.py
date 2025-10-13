8fe5e184b1f5ab8f8c1510e2e6f100eca548b25d
JonasHoever <jonas.hoever@edeka.de>
2025-10-13 12:06:52 +0200

    push2

--- server/src/db_setup.py
+++ server/src/db_setup.py
@@ -8,7 +8,7 @@ class Init():
     
     def create_tables(self):
         self.cursor.execute("CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key NOT NULL, nfc_tag VARCHAR(255) unique NOT NULL, pin VARCHAR(255) DEFAULT NULL, name varchar(255), surname varchar(255), active tinyint default 1, creation_stamp datetime DEFAULT CURRENT_TIMESTAMP);")
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS user_data(session_id INT AUTO_INCREMENT PRIMARY KEY, user_id INT, checkin_time DATETIME, checkout_time DATETIME, date DATE DEFAULT CURRENT_DATE, status tinyint DEFAULT 1, session_duration DATETIME, FOREIGN KEY (user_id) REFERENCES users(id))")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS user_data(session_id INT AUTO_INCREMENT PRIMARY KEY, user_id INT, checkin_time DATETIME, checkout_time DATETIME, date DATE DEFAULT CURRENT_DATE, status tinyint DEFAULT 1, session_duration FLOAT, FOREIGN KEY (user_id) REFERENCES users(id))")
     def del_tables(self):
         self.cursor.execute("DROP TABLE IF EXISTS user_data")
         self.cursor.execute("DROP TABLE IF EXISTS users;")
